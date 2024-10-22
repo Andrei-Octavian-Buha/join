ea2400ceba2544f2d9b8123745ae596834da469c
Niklas Lampe <125186474+NiklasLa46@users.noreply.github.com>
2024-10-22 12:54:44 +0200

    added Buttons + JS-Code for button animation

---
+++ js/summary_user.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.getElementById('summary-icon-pen');
+    const originalSrc = './assets/img/summary-button-pen.png';
+    const hoverSrc = './assets/img/summary-button-pen-h.png';
+    const anchor = document.querySelector('.stat-button-to-do');
+ 
+    anchor.addEventListener('mouseenter', function() {
+        image.src = hoverSrc; 
+    });
+
+    anchor.addEventListener('mouseleave', function() {
+        image.src = originalSrc; 
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.getElementById('summary-icon-check');
+    const originalSrc = './assets/img/summary-button-check.png';
+    const hoverSrc = './assets/img/summary-button-check-h.png';
+    const anchor = document.querySelector('.stat-button-done');
+ 
+    anchor.addEventListener('mouseenter', function() {
+        image.src = hoverSrc; 
+    });
+
+    anchor.addEventListener('mouseleave', function() {
+        image.src = originalSrc; 
+    });
+});
