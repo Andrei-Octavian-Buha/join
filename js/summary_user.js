7c755dac8e8d4b1572439b52c8f283e9d286b4b0
Niklas Lampe <125186474+NiklasLa46@users.noreply.github.com>
2024-10-23 12:43:14 +0200

    finished summary buttons html+css

--- js/summary_user.js
+++ js/summary_user.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const image = document.getElementById('summary-icon-pen');
     const originalSrc = './assets/img/summary-button-pen.png';
     const hoverSrc = './assets/img/summary-button-pen-h.png';
-    const anchor = document.querySelector('.stat-button-to-do');
+    const anchor = document.querySelector('.stats-button-to-do');
  
     anchor.addEventListener('mouseenter', function() {
         image.src = hoverSrc; 
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const image = document.getElementById('summary-icon-check');
     const originalSrc = './assets/img/summary-button-check.png';
     const hoverSrc = './assets/img/summary-button-check-h.png';
-    const anchor = document.querySelector('.stat-button-done');
+    const anchor = document.querySelector('.stats-button-done');
  
     anchor.addEventListener('mouseenter', function() {
         image.src = hoverSrc; 
