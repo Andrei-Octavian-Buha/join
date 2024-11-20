d7f43e3d6899d8821b3209c7c645016885e13699
René.Götze <goetze.rene@web.de>
2024-11-20 13:58:45 +0100

    add task color badge

--- js/add_task.js
+++ js/add_task.js
@@ -13,33 +13,81 @@ function subtasktrigger() {
   startEventListner();
 }
 
-async function loadContacts() {
+async function loadContactsData() {
   let ContactResponse = await getAllContacts("contacts");
   let UserKeyArray = Object.keys(ContactResponse);
-  let dropdown = document.getElementById("dropDownBodyId");
-  dropdown.innerHTML = "";
-  contacts = UserKeyArray.map((id) => ({
+  return UserKeyArray.map((id) => ({
     id: id,
     cont: ContactResponse[id],
   }));
-  contacts.forEach((element) => {
-    let container = document.createElement("div");
-    container.id = `ContainerID${element.id}`;
-    container.classList.add("dropDownContactContainer");
-    container.innerHTML = `
-      <div class="dropDownContactName">
-        <p class="firstLetterCircle">SM</p>
-        <p class="dropDownFullName">${element.cont.name}</p>
-      </div>
-      <input
-        type="checkbox"
-        class="contactCheckbox"
-        id="CheckboxID${element.id}"
-      />
-    `;
-    dropdown.appendChild(container);
-    startEvent(element);
+}
+
+function sortContacts(contacts) {
+  return contacts.sort((a, b) => {
+    let nameA = a.cont.name.toUpperCase(); // Großbuchstaben für den Vergleich
+    let nameB = b.cont.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1; // a kommt vor b
+    }
+    if (nameA > nameB) {
+      return 1; // b kommt vor a
+    }
+    return 0; // Wenn die Namen gleich sind
   });
+}
+
+function createContactElement(element) {
+  let container = document.createElement("div");
+  container.id = `ContainerID${element.id}`;
+  container.classList.add("dropDownContactContainer");
+
+  // Extrahiere die Initialen des Kontakts
+  let initials = element.cont.name.split(" ").map(word => word[0].toUpperCase()).join("").slice(0, 2);
+
+  // Hol die Farbe für die Initialen
+  let color = getColorForInitial(initials[0]); // Verwende nur den ersten Buchstaben für die Farbe
+
+  container.innerHTML = `
+    <div class="dropDownContactName">
+      <p class="firstLetterCircle" style="background-color: ${color};">${initials}</p>
+      <p class="dropDownFullName">${element.cont.name}</p>
+    </div>
+    <input
+      type="checkbox"
+      class="contactCheckbox"
+      id="CheckboxID${element.id}"
+    />
+  `;
+  return container;
+}
+
+function renderContacts(contacts) {
+  let dropdown = document.getElementById("dropDownBodyId");
+  dropdown.innerHTML = ""; // Existierende Kontakte entfernen
+
+  contacts.forEach((element) => {
+    let container = createContactElement(element);
+    dropdown.appendChild(container);
+    startEvent(element); // Starte das Event für diesen Kontakt
+  });
+}
+
+async function loadContacts() {
+  let contactsData = await loadContactsData(); // Daten abrufen
+  let sortedContacts = sortContacts(contactsData); // Kontakte sortieren
+  renderContacts(sortedContacts); // Kontakte rendern
+}
+
+function getColorForInitial(initial) {
+  const colors = {
+      A: '#FF5733', B: '#FFBD33', C: '#DBFF33', D: '#75FF33', E: '#33FF57', 
+      F: '#33FFBD', G: '#33DBFF', H: '#3375FF', I: '#5733FF', J: '#BD33FF', 
+      K: '#FF33DB', L: '#FF3375', M: '#FF3333', N: '#FF6633', O: '#FF9933', 
+      P: '#FFCC33', Q: '#FFFF33', R: '#CCFF33', S: '#99FF33', T: '#66FF33', 
+      U: '#33FF66', V: '#33FF99', W: '#33FFCC', X: '#33FFFF', Y: '#33CCFF', 
+      Z: '#3399FF'
+  };
+  return colors[initial] || '#333333'; 
 }
 
 function startEvent(contactId) {
